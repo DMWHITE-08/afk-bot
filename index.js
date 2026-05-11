5edf6a8994f7985ec053488de1219b50a5e265f4
DMWHITE-08 <albinrosepaul7@gmail.com>
2026-05-11 12:23:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 const mineflayer = require('mineflayer')
 
+function createBot() {
+
 const bot = mineflayer.createBot({
-  host: 'maincraftandme.aternos.me',
-  port: 29449,
-  username: 'AFK_Bot',
-  auth: 'offline'
+    host: 'maincraftandme.aternos.me',
+    port: 29449,
+    username: 'AFK_Bot',
+    auth: 'offline'
 })
 
 bot.on('spawn', () => {
-  console.log('Bot joined')
+    console.log('Bot joined')
 
-  setInterval(() => {
-    bot.setControlState('jump', true)
+    setInterval(() => {
 
-    setTimeout(() => {
-      bot.setControlState('jump', false)
-    }, 500)
-  }, 30000)
+        const actions = ['forward', 'back', 'left', 'right']
+        const action = actions[Math.floor(Math.random() * actions.length)]
+
+        bot.setControlState(action, true)
+        bot.setControlState('jump', true)
+
+        setTimeout(() => {
+            bot.clearControlStates()
+        }, 2000)
+
+    }, 10000)
 })
+
+bot.on('end', () => {
+    console.log('Disconnected, reconnecting...')
+    setTimeout(createBot, 5000)
+})
+
+bot.on('error', err => console.log(err))
+}
+
+createBot()
